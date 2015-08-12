5cbc9f3b3618645c7b671712c4c4ae79f1990bdb
Tianon Gravi <admwiggin@gmail.com>
2015-08-12 13:03:44 -0700

    Add initial .travis.yml

--- update.sh
+++ update.sh
@@ -9,8 +9,14 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+
+travisEnv=
 for version in "${versions[@]}"; do
 	latest=$(curl -sSL 'https://owncloud.org/changelog/' |tac|tac| grep -o -m 1 "\(Version\|Release\) ${version}.[[:digit:]]\+" | sed -rn 's/(Version|Release) (.*)/\2/p')
 	sed -ri -e 's/^(ENV OWNCLOUD_VERSION) .*/\1 '"$latest"'/' "$version/Dockerfile"
+	
+	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
 
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
