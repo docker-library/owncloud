6fe2ae60b141d3a2badf0cb260d834c2f6a6299a
pierreozoux <pierre@ozoux.net>
2015-09-23 09:51:54 +0100

    Updates scripts for fpm and apache

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -17,18 +17,27 @@ url='git://github.com/docker-library/owncloud'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
-	commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-	fullVersion="$(grep -m1 'ENV OWNCLOUD_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
-	
-	versionAliases=()
-	while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
-		versionAliases+=( $fullVersion )
-		fullVersion="${fullVersion%[.-]*}"
-	done
-	versionAliases+=( $version ${aliases[$version]} )
-	
-	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
+	for variant in apache fpm; do
+		commit="$(cd "$version/$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
+		fullVersion="$(grep -m1 'ENV OWNCLOUD_VERSION ' "$version/$variant/Dockerfile" | cut -d' ' -f3)"
+
+		versionAliases=()
+		while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+			versionAliases+=( $fullVersion )
+			fullVersion="${fullVersion%[.-]*}"
+		done
+		versionAliases+=( $version ${aliases[$version]} )
+
+		echo
+		for va in "${versionAliases[@]}"; do
+			echo "$va-$variant: ${url}@${commit} $version/$variant"
+			if [ "${variant}" == "apache" ]; then
+				echo "$va: ${url}@${commit} $version/$variant"
+			else
+				if [ "$va" == "latest" ]; then
+					echo "$variant: ${url}@${commit} $version/$variant"
+				fi
+			fi
+		done
 	done
 done
