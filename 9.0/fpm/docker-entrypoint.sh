58e887ac4c2326160592455479166579410862dd
Tianon Gravi <admwiggin@gmail.com>
2018-02-21 12:45:55 -0800

    Remove EOL 9.0 series

--- 9.0/fpm/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ ! -e '/var/www/html/version.php' ]; then
-	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
-	chown -R www-data /var/www/html
-fi
-
-exec "$@"
